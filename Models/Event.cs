fd8c98164d74e9897e7f0635272b71e965a36b5f
adnan123648 <qadiradnan152@gmail.com>
2025-06-19 16:18:13 +0500

    Add files via upload

---
+++ Models/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace StudentEventAPI.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Venue { get; set; }
+
+        [Required]
+        [JsonPropertyName("eventDate")] // 👈 Fixes mapping
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<EventParticipant> EventParticipants { get; set; } = new List<EventParticipant>();
+    }
+}
